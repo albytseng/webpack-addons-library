a641dd34a0883fed914b6a1f7d43e43c6fab7cca
Alby Tseng <albytseng@gmail.com>
2018-03-19 00:04:41 -0700

    Fix typo

--- src/make-common-config.js
+++ src/make-common-config.js
@@ -73,7 +73,7 @@ module.exports = function (answer) {
           ...nodeConfig,
           output: {
             ...baseConfig.output,
-            filename: getNodeOutputName(outputName)},
+            filename: getNodeOutputName(outputName),
           },
         },
       ];
